ae0d3811a8465788384422a5a93b7a1cb26e6fd0
Pablo Pessanha <pablopessanh@gmail.com>
2023-04-22 18:18:04 -0300

    feat: adds loading component

---
+++ components/Loading.vue
@@ -0,0 +1,6 @@
+<template>
+  <div
+    class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] mt-1"
+    role="status"
+  />
+</template>
