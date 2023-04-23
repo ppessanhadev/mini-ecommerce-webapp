6ac5a9ee7f914b240525069bf93fad328a86551a
Pablo Pessanha <pablopessanh@gmail.com>
2023-04-22 23:48:52 -0300

    feat: adds carts quantity on header cart

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+  const state = useProductsStore();
+</script>
+
 <template>
   <header class="flex m-auto p-4 lg:w-[960px] justify-between items-center">
     <NuxtLink class="text-green-500 italic text-2xl" to="/">
@@ -7,8 +11,8 @@
     <NuxtLink class="relative" to="/cart">
       <SVGCart class="h-8 w-8 fill-zinc-700" />
 
-      <p class="absolute w-5 h-5 bg-gray-100 border-2 border-zinc-700 right-0 bottom-0 rounded-full text-center text-xs text-zinc-700 font-bold">
-        2
+      <p v-if="state.cartsQuantity" class="absolute w-5 h-5 bg-gray-100 border-2 border-zinc-700 right-0 bottom-0 rounded-full text-center text-xs text-zinc-700 font-bold">
+        {{ state.cartsQuantity }}
       </p>
     </NuxtLink>
   </header>
