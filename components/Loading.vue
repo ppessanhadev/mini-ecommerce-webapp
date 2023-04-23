a6933396107f3b705d4959ecafb5f366498a10e0
Pablo Pessanha <pablopessanh@gmail.com>
2023-04-23 02:05:39 -0300

    refactor: moves section block inside on loading

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,6 +1,8 @@
 <template>
-  <div
-    class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] mt-1"
-    role="status"
-  />
+  <section class="fixed flex justify-center items-center w-full h-screen">
+    <div
+      class="inline-block w-16 h-16 text-zinc-700 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] mt-1"
+      role="status"
+    />
+  </section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
     const cart = localStorage.getItem('cart');
 
     if (cart) {
-      state.setCart(JSON.parse(cart));
+      state.cart = JSON.parse(cart);
     }
     state.loading = false;
   });
@@ -21,9 +21,7 @@
 <template>
   <Header />
 
-  <section v-if="state.loading" class="fixed flex justify-center items-center w-full h-screen">
-    <Loading class="w-16 h-16 text-zinc-700" />
-  </section>
+  <Loading v-if="state.loading" />
 
   <main v-else class="flex flex-col items-center md:grid md:grid-cols-2 lg:grid-cols-4 md:place-items-center m-auto lg:w-[1024px] gap-2 mt-12">
     <ProductCard v-for="product in state.products" :key="product.id" :product="product" />
